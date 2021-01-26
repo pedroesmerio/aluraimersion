a7d1dc7c614529c32224957e67b0d10909eb3b2b
pedroesmerio <p.h.esmerio@gmail.com>
2021-01-26 05:23:25 -0300

    Aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../assets/components/Widget/Widget.js';
+import QuizLogo from '../assets/components/QuizLogo/QuizLogo.js';
+import QuizBackground from '../assets/components/QuizBackground/QuizBackground.js';
+import Footer from '../assets/components/Footer/Footer.js';
+import GitHubCorner from '../assets/components/GitHubCorner/GitHubCorner.js';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Header>
+            <p>{db.title}</p>
+          </Widget.Header>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz do Narutinho</h1>
+            <p>{db.title}</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/pedroesmerio" />
+    </QuizBackground>
+  );
 }
